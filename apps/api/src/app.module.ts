f5bdaf96c5d50dc720e7be9e408d515357c487db
Jan Sobanski <sobanski.jan2@gmail.com>
2024-03-01 23:43:45 +0100

    Use the config service + configure cors

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from './config';
 import { AppService } from './app.service';
 
 @Module({
   providers: [AppService],
+  imports: [ConfigModule],
 })
 export class AppModule {}
